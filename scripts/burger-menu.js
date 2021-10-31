501e06aa8a606e29a717248d5549d6e9d0be41f8
MedusCode <WoWandreev@gmail.com>
2021-10-31 22:08:37 +0300

    feat: add footer

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -2,23 +2,29 @@ const list = document.querySelector('.header__list');
 const listElements = document.querySelectorAll('.header__list-element')
 const burgerContainer = document.querySelector('.header__burger-container');
 const burger = document.querySelector('.header__burger');
+const themeSwitcher = document.querySelector('.header__theme-switcher')
 
+function showElement (element) {
+  element.style.visibility = 'visible';
+  element.style.opacity = 1;
+}
 
-function closeListSmoothy () {
-  list.style.opacity = 0;
+function hideElement (element) {
+  element.style.opacity = 0;
     setTimeout(() => {
-      list.classList.remove('header__list_opened');
-      list.removeAttribute('style');
+      element.style.visibility = 'hidden';
     }, 300);
 }
 
 function closeOrOpenNavigation () {
   burger.classList.toggle('header__burger_opened');
-  if (list.classList.contains('header__list_opened')) {
-    closeListSmoothy();
+  if (burger.classList.contains('header__burger_opened')) {
+    showElement(list);
+    showElement(themeSwitcher)
   }
   else {
-    list.classList.add('header__list_opened');
+    hideElement(list);
+    hideElement(themeSwitcher);
   }
 }
 
